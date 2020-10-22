fa7a84534ec808e10bcfbba92dd40ad0ecb3fbe8
jakeFromm <jacobfromm@gmail.com>
2020-10-22 09:25:54 -0400

    new activity functionality

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const moodDiv = document.querySelector("#all-moods-container")
     const moodList = document.createElement("ul")
     const MOODS_URL = "http://localhost:3000/moods/"
+    const ACTIVITIES_URL = 'http://localhost:3000/activities/'
+    let actArr = []
+
+    // const background = document.body
+    // background.classList.add("background-image")
 
     function getMoods(){
         const moodsDiv = document.querySelector('#all-moods-container')
         moodsDiv.innerHTML = ""
         fetch(MOODS_URL)
             .then(response => response.json())
-            .then(moods => renderMoods(moods))
+            // .then(moods => renderMoods(moods))
             .then(makeChart)
     }
 
@@ -52,7 +57,25 @@ document.addEventListener('DOMContentLoaded', () => {
                     .then(_newMood => 
                         makeChart())
 
-                
+                form.reset()
+            } else if (e.target.matches("#new-activity-form")) {
+                e.preventDefault()
+                const nowDate = new Date(Date.now())
+                const form = e.target
+                const newAct = {name: form.activity.value, date: nowDate, user_id: 1}
+
+                const options = {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                        "accept": "application/json"
+                    },
+                    body: JSON.stringify(newAct)
+                }
+                fetch(ACTIVITIES_URL, options)
+                    .then(response => response.json())
+                    .then(getActivities())
+                    .then(console.log(actArr[actArr.length]))
                 form.reset()
             }
         })
@@ -129,20 +152,96 @@ document.addEventListener('DOMContentLoaded', () => {
                         }
                     }
                 });
+                document.getElementById("myChart").onclick = e => {
+                    let activePoints = myChart.getElementsAtEvent(e)
+                    if (activePoints.length > 0) {
+                        let clickedElementIndex = activePoints[0]["_index"]
+                        let label = myChart.data.labels[clickedElementIndex]
+                        let value = myChart.data.datasets[0].data[clickedElementIndex]
+                        const button = document.createElement("button")
+                        button.textContent = "Delete Mood"
+                        button.dataset.id = myChart.data.datasets[0].data[clickedElementIndex].id
+                        button.classList.add("delete-mood-btn")
+                        const editMoodDiv = document.querySelector("#edit-mood-div")
+                        editMoodDiv.append(button)
+                    }   
+                }
             } )
 
             const populateData = (moods) => {
                 let data = []
                 for (const mood of moods) {
                     let date = new Date(mood.date)
-                    data.push({x: date, y: mood.mood_level})
+                    data.push({x: date, y: mood.mood_level, id: mood.id})
                 }
                 const sortedData = data.sort((a, b) => b.x - a.x)
                 return sortedData
             }
         
     }
-    // clickHandler()
+
+    function clickHandler(){
+        document.addEventListener("click", e => {
+            if (e.target.matches(".delete-mood-btn")){
+                const button = e.target
+                const moodId = button.dataset.id
+                const options = {
+                    method: "DELETE"
+                }
+                fetch(MOODS_URL + moodId, options)
+                    .then(_data => makeChart())
+                button.remove()
+            } else if (e.target.matches("#new-act-btn")) {
+                const actText = document.querySelector('#random-act-text')
+                actText.textContent = actArr[getRandomInt(actArr.length)]
+            }
+        })
+    }
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    const getActivities = () => {
+        fetch(ACTIVITIES_URL)
+            .then(response => response.json())
+            .then(activities => renderActivities(activities))
+
+    }
+
+    const renderActivities = (activities) => {
+        const activitiesContainer = document.querySelector('#activities-container')
+        const actList = document.createElement("ul")
+        for (const activity of activities) {
+            actArr.push(activity.name)
+        }
+        const actText = document.querySelector('#random-act-text')
+        actText.textContent = actArr[getRandomInt(actArr.length)]
+    }
+
+    // const renderActivity = (activities) => {
+    //     // const activitiesContainer = document.querySelector('#activities-container')
+    //     // const actList = document.createElement("ul")
+    //     // for (const activity in activities){
+    //     //     const activityItem = document.createElement('li')
+    //     //     console.log(activity)
+    //     //     // activityItem.textContent = activity.name
+    //     //     // actList.append(activityItem)
+    //     // }
+    //     //     activitiesContainer.append(actList)
+    // }
+    // const activityButton = () => {
+    //     const renderActButton = document.querySelector('#activity-container')
+    //     const activityList = document.createElement('div')
+    //     activityList.id = 'activity-list'
+    //     const actButton = document.createElement('button')
+    //     actButton.innerHTML = 'Click Here to see Activities!'
+    //     actButton.addEventListener('click', e => {
+    //         getActivities()
+    //     })
+    //     renderActButton.append(actButton, activityList)
+    // }
+
+    clickHandler()
     submitHandler()
     getMoods()
+    getActivities()
 })
